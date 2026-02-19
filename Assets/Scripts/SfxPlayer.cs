836bc4a2c34d0c794aee23d47099d786816ed794
Xoggas <sashakor16@gmail.com>
2026-02-19 13:21:58 +0300

    Added an SFX player

---
+++ Assets/Scripts/SfxPlayer.cs
@@ -0,0 +1,24 @@
+﻿using AxGrid.Base;
+using AxGrid.Model;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public sealed class SfxPlayer : MonoBehaviourExtBind
+{
+    [SerializeField]
+    private AudioClip _clickSfx;
+
+    private AudioSource _audioSource;
+
+    [OnAwake]
+    private void OnAwake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    [Bind(Events.Scrolled)]
+    private void PlayClickSound()
+    {
+        _audioSource.PlayOneShot(_clickSfx);
+    }
+}
